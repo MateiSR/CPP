55556ea2dfbf19924929ead633cea17d4de4874d
Matei Radu <37928133+MateiSR@users.noreply.github.com>
2021-09-29 17:34:46 +0300

    Update interclasare-intersectie.cpp

--- algorithms/interclasare-intersectie.cpp
+++ algorithms/interclasare-intersectie.cpp
@@ -1,13 +1,13 @@
-    p = 0;
-    int i = 0, j = 0;
-    while(i <= n && j <= m)
-        if(a[i]<b[j])
-            c[p++] = a[i++];
-        else if(a[i] > b[j])
-            c[p++] = b[j++];
-        else
-            i++;
-    while(i <= n)
+int i = 0, j = 0;
+p = 0;
+while(i <= n && j <= m)
+    if(a[i]<b[j])
         c[p++] = a[i++];
-    while(j <= m)
+    else if(a[i] > b[j])
         c[p++] = b[j++];
+    else
+        i++;
+while(i <= n)
+    c[p++] = a[i++];
+while(j <= m)
+    c[p++] = b[j++];
