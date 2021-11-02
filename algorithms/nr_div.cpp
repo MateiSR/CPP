d9c0d2bd1ab48a44e3c2537b513cee0c72dc371b
Matei Radu <37928133+MateiSR@users.noreply.github.com>
2021-11-02 18:41:49 +0200

    Update nr_div.cpp

--- algorithms/nr_div.cpp
+++ algorithms/nr_div.cpp
@@ -1,8 +1,8 @@
 int nrdiv(int n) {
     int cnt = 0;
     for (int d = 1; d  * d <= n; d++) {
-        if (n % d == 0) cnt++;
-        if (d * d < n) cnt ++;
+        if (n % d == 0) cnt++; // d - divizor a lui n
+        if (d * d < n) cnt ++; // n / d - divizor a lui n
     }
     return cnt;
 }
