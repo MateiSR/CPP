1086bf033d498ea9646c5e1990ba70d12ff1791a
Matei Radu <37928133+MateiSR@users.noreply.github.com>
2022-09-09 15:05:47 +0300

    Update nrdirect.cpp

--- algorithms/nrdirect.cpp
+++ algorithms/nrdirect.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 // Numarul direct!!!
 int main() {
-    int n, x = 0, k = 1;
+    int n, x = 0, p = 1;
     cin >> n;
-    do {
-        if (n) x += k * (n % 10);
-        k *= 10;
+    while (n != 0) {
+        if (n) x += p * (n % 10);
+        p *= 10;
         n = n /10;
-    } while (n != 0);
+    }
     cout << x;
 }
